83e0d52486f6f27782fc4c7ae6e8e3a4f1d2b80d
Thipwimon <d.leisure7@gmail.com>
2025-11-19 17:13:50 +0700

    update task01 task02

--- task01.ts
+++ task01.ts
@@ -1,3 +1,19 @@
 // TODO-01: Use if/else to print "Even" or "Odd"
 
 const number = 15;
+
+// with Genmate F
+// % is modulo operator :คืนค่าเศษจากการหาร
+if(number %2 === 0) {
+    console.log("Even");
+} else{
+    console.log("Odd");
+}
+
+
+//solutio of US
+// if(number %2) {
+//     console.log("Odd");
+// } else{
+//     console.log("Even");
+// }
--- task02.ts
+++ task02.ts
@@ -1,5 +1,19 @@
 // TODO-02: Use if/else to check if it's a number
 // (hint: typeof or isNaN)
 
-const userInput = "42";
+const userInput = NaN;
 // Try changing to "hello", 10, null, NaN
+if(typeof Number(userInput) === "number" && !isNaN(Number(userInput))){
+    console.log("it's a number");
+
+} else{
+    console.log("It's not a number");
+}
+
+
+// Solution of Itt
+// if(typeof userInput === "number" && !Number.isNaN(userInput)){
+//     console.log(`T :$(userInput) <- This is Number`);
+// }else{
+//     console.log(`F :$(userInput) <- This is not number /is NaN`);
+// }
